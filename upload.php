bbb61a45428bdd773bbc959971258b4e81d5b388
zsz12251665 <zsz12251665@163.com>
2020-04-22 20:48:06 +0800

    Beautify upload page

--- upload.php
+++ upload.php
@@ -1,19 +1,35 @@
+<!DOCTYPE html>
+<html lang="zh">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+		<meta http-equiv="Cache-Control" content="no-cache" />
+		<meta name="viewport" content="width=device-width" />
+		<!-- <link rel="shortcut icon" href="/favicon.ico" /> -->
+		<link rel="stylesheet" type="text/css" href="/css/style.css">
+		<title>SCUT 2019计科全英联合班作业提交系统</title>
+	</head>
+	<body>
+		<header></header>
+		<main>
+			<h1>SCUT 2019计科全英联合班作业提交系统</h1>
 <?php
-	echo "系统仍在施工中！！！<br />";
-	// echo "StuName: " . $_POST["StuName"] . "<br />";
-	// echo "StuNumber: " . $_POST["StuNumber"] . "<br />";
-	// echo "WorkTitle: " . $_POST["WorkTitle"] . "<br />";
+	echo "<p>系统仍在施工中！！！</p>";
+	// echo "<p>StuName: " . $_POST["StuName"] . "</p>";
+	// echo "<p>StuNumber: " . $_POST["StuNumber"] . "</p>";
+	// echo "<p>WorkTitle: " . $_POST["WorkTitle"] . "</p>";
 	require "sql.php";
 	if (!isStudent($_POST["StuName"], $_POST["StuNumber"]))
-		die($_POST["StuName"] . " (" . $_POST["StuNumber"] . ") is not our student! ");
+	{
+		die("<p>" . $_POST["StuName"] . " (" . $_POST["StuNumber"] . ") is not our student! </p>");
+	}
 	if($_FILES["WorkFile"]["error"])
 	{
-		echo "Error: " . $_FILES["WorkFile"]["error"];
+		die("<p>Error: " . $_FILES["WorkFile"]["error"] . "</p>");
 	}
 	else
 	{
-		// echo "Upload: " . $_FILES["WorkFile"]["name"] . "<br />";
-		// echo "Size: " . $_FILES["WorkFile"]["size"] . " Byte(s)<br />";
+		// echo "<p>Upload: " . $_FILES["WorkFile"]["name"] . "</p>";
+		// echo "<p>Size: " . $_FILES["WorkFile"]["size"] . " Byte(s)</p>";
 		require "local.php";
 		if (!file_exists($upload_directory . $_POST["WorkTitle"] . "/"))
 		{
@@ -22,7 +38,11 @@
 		$savingLocation = $upload_directory . $_POST["WorkTitle"] . "/" . $_POST["StuName"] . "-" . $_POST["StuNumber"] . "." . strtolower(pathinfo($_FILES["WorkFile"]["name"], PATHINFO_EXTENSION));
 		if(move_uploaded_file($_FILES["WorkFile"]["tmp_name"], $savingLocation))
 		{
-			echo "Upload successfully! ";
+			echo "<p>Upload successfully! </p>";
 		}
 	}
 ?>
+		</main>
+		<footer></footer>
+	</body>
+</html>
