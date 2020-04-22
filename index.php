f98db59700914d8e8e9b5f0e7ae33f7445e90e5f
zsz12251665 <zsz12251665@163.com>
2020-04-22 20:28:06 +0800

    Spilit css stylesheet

--- index.php
+++ index.php
@@ -5,59 +5,13 @@
 		<meta http-equiv="Cache-Control" content="no-cache" />
 		<meta name="viewport" content="width=device-width" />
 		<!-- <link rel="shortcut icon" href="/favicon.ico" /> -->
+		<link rel="stylesheet" type="text/css" href="/css/style.css">
 		<title>SCUT 2019计科全英联合班作业提交系统</title>
-		<style type="text/css">
-body
-{
-	height: 100vh;
-	margin: 0;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-}
-header, footer
-{
-	flex-grow: 1;
-}
-section
-{
-	margin: 0.5em;
-}
-input{
-	border: 1px solid #ccc;
-	padding: 7px 0px;
-	border-radius: 3px;
-	padding-left:5px;
-	-webkit-box-shadow: inset 0 1px 1px rgba(0,0,0,.075);
-	box-shadow: inset 0 1px 1px rgba(0,0,0,.075);
-	-webkit-transition: border-color ease-in-out .15s,-webkit-box-shadow ease-in-out .15s;
-	-o-transition: border-color ease-in-out .15s,box-shadow ease-in-out .15s;
-	transition: border-color ease-in-out .15s,box-shadow ease-in-out .15s
-}
-input:focus{
-	border-color: #66afe9;
-	outline: 0;
-	-webkit-box-shadow: inset 0 1px 1px rgba(0,0,0,.075),0 0 8px rgba(102,175,233,.6);
-	box-shadow: inset 0 1px 1px rgba(0,0,0,.075),0 0 8px rgba(102,175,233,.6)
-}
-select{
-	border: 1px solid #ccc;
-	padding: 7px 0px;
-	border-radius: 3px;
-	padding-left:5px;
-}
-h1{
-	align="center";
-}
-body{
-	background="img/background.png";
-}
-		</style>
 	</head>
-	<body >
+	<body>
 		<header></header>
 		<main>
-			<h1 >SCUT 2019计科全英联合班作业提交系统</h1>
+			<h1>SCUT 2019计科全英联合班作业提交系统</h1>
 			<form action="upload.php" method="post" enctype="multipart/form-data">
 				<section>
 					<label for="StuName">学生姓名：</label>
