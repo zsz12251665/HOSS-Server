6201e3d72f1165561b60b7e3e9867468569f76de
zsz12251665 <zsz12251665@163.com>
2020-06-16 23:51:16 +0800

    Update homework.php

--- homework.php
+++ homework.php
@@ -2,12 +2,6 @@
 	$studentName = htmlspecialchars($_POST["StuName"], ENT_QUOTES);
 	$studentNumber = htmlspecialchars($_POST["StuNumber"], ENT_QUOTES);
 	require "sql.php";
-	// If the student is in the database
-	if ($studentName && $studentNumber && !selectInMysql("students", array("name" => $studentName, "number" => $studentNumber)))
-	{
-		header("HTTP/1.1 403 Forbidden");
-		die("Authorization Error: " . $studentName . " (" . $studentNumber . ") is not our student! ");
-	}
 	// Get the list from MySQL database
 	$homeworkList = array_reverse(selectInMysql("homeworks"));
 	// Get the number of homeworks submitted
@@ -19,30 +13,22 @@
 			$homework["directory"] = $homework["title"];
 		}
 		$localDirectory = $upload_directory . $homework["directory"] . "/";
-		$fileList = (file_exists($localDirectory) && is_dir($localDirectory)) ? scandir($localDirectory) : array(".", "..");
+		$fileList = file_exists($localDirectory) && is_dir($localDirectory) ? scandir($localDirectory) : array(".", "..");
 		$homework["count"] = count($fileList) - 2;
-		if ($studentName && $studentNumber)
+		$homework["checked"] = false;
+		if ($studentName && $studentNumber && $homework["count"] > 0)
 		{
-			$filename = $studentNumber . "-" . $studentName;
-			$homework["isSubmitted"] = false;
-			foreach ($fileList as $file)
+			foreach ($fileList as $filename)
 			{
-				if (strpos($file, $filename) === 0)
+				if (strpos($filename, $studentNumber . "-" . $studentName) === 0)
 				{
-					$homework["isSubmitted"] = true;
+					$homework["checked"] = true;
 					break;
 				}
 			}
 		}
 	}
 	// Output the information in JSON
-	if ($studentName && $studentNumber)
-	{
-		header("HTTP/1.1 200 OK");
-	}
-	else
-	{
-		header("HTTP/1.1 203 Non-Authoritative Information");
-	}
+	header("HTTP/1.1 200 OK");
 	die(json_encode($homeworkList));
 ?>
