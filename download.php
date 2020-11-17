cac7e778e511f9e1056b35b308fa12fbb85f0cb4
zsz12251665 <zsz12251665@hotmail.com>
2020-11-17 16:48:28 +0800

    Add unsubmission counter

--- download.php
+++ download.php
@@ -22,13 +22,32 @@
 		header("HTTP/1.1 500 Internal Server Error");
 		die("Fail zip: ZipArchive failure! ");
 	}
+	require "sql.php";
+	$studentList = selectInMysql("students", array());
+	$submitted = array_fill(0, count($studentList), false);
 	foreach (scandir($workDirectory) as $fileName)
 	{
 		if ($fileName != "." && $fileName!= ".." && !is_dir($workDirectory . $fileName))
 		{
 			$zip->addFile($workDirectory . $fileName, $fileName);
+			foreach ($studentList as $index => $student)
+			{
+				if (strpos($fileName, $student["number"] . "-" . $student["name"]) === 0)
+				{
+					$submitted[$index] = true;
+				}
+			}
 		}
 	}
+	$tmpFilename = tempnam(sys_get_temp_dir(), "HOSS");
+	foreach ($studentList as $index => $student)
+	{
+		if (!$submitted[$index])
+		{
+			file_put_contents($tmpFilename, $student["number"] . "," . $student["name"] . "\n", FILE_APPEND);
+		}
+	}
+	$zip->addFile($tmpFilename, "missing.csv");
 	$zip->close();
 	// Output the zip file
 	header("Cache-Control: no-store");
